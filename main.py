81c91d2e33c76e0cfd903d892b122dd004718b61
ohtjqkd <ohtjqkd@gmail.com>
2021-03-28 17:07:13 +0900

    modified:   __pycache__/main.cpython-38.pyc 	modified:   __pycache__/scrapper.cpython-38.pyc 	modified:   build/main/Analysis-00.toc 	modified:   build/main/EXE-00.toc 	modified:   build/main/PKG-00.pkg 	modified:   build/main/PKG-00.toc 	modified:   build/main/PYZ-00.pyz 	modified:   build/main/PYZ-00.toc 	modified:   build/main/warn-main.txt 	modified:   build/main/xref-main.html 	new file:   dist/Zun1121.exe 	modified:   dist/main.exe 	modified:   main.py 	modified:   main.spec 	new file:   scrapper/__pycache__/homepageScrapper.cpython-38.pyc 	new file:   scrapper/__pycache__/scrapper.cpython-38.pyc 	renamed:    homepageScrapper.py -> scrapper/homepageScrapper.py 	renamed:    scrapper.py -> scrapper/scrapper.py

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-import homepageScrapper
-import downloader
-import requests
-import scrapper
-keyword = input()
+import scrapper.homepageScrapper as homepageScrapper
+
+import time
 # print(scrapper.scrapJobKr(keyword))
 # print(scrapper.testSel('https://www.saramin.co.kr/zf_user/company-info/view?csn=ZFFwTDRyK3dRNklvY2c2dS9TQWVFZz09&popup_yn=y'))
-print(homepageScrapper.saraminScrap(keyword))
+keyword = input()
+try:
+    print(homepageScrapper.saraminScrap(keyword))
+except Exception as e:
+    print(e)
+    time.sleep(10)
+
+while input() == "exit":
+    break
 # print(homepageScrapper.jobkoreaScrap(keyword))
 # print(requests.get("https://www.saramin.co.kr/zf_user/jobs/relay/view?isMypage=no&rec_idx=39538120&recommend_ids=eJxNz0EOBCEIRNHTzB4KLGDdB%2FH%2Btxg7Jg3unnxjsKIlwjYFv3hscPu9WJDqeYB%2BZuG8tCD8I1927AUdXMzzfi%2FqpWYxPtIrQpop53Qs5x9vZobmiE0w4jL46n9TltdY0ROTC6ojViJ6BSojO66E8%2BUfBFlENA%3D%3D&view_type=search&searchword=javascript&searchType=search&gz=1&t_ref_content=generic&t_ref=search&paid_fl=n#seq=0").text)
 # print(getBySelenium(keyword))
